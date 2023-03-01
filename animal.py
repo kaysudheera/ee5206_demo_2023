348b996ee0dc9ac9c047a50ca78bece0baf4a9f5
Kushan Sudheera Kalupahana Liyanage <kaysudheera@gmail.com>
2023-03-01 19:16:00 +0530

    dog function

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def dog():
+    print('baw')
+
 def default():
     print('Hellp')
 
 def main():
-    default()
+    if sys.argv[1] == 'dog':
+        dog()
+    else:
+        default()
 
 if __name__ == '__main__':
     main()
